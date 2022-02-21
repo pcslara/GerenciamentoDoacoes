710b8173e864340b177a6dd29807b4bd1cd0cc2d
Douglas Cardoso <douglascardoso@cos.ufrj.br>
2022-02-21 01:27:19 -0300

    user get demanda centro

---
+++ user/model/user_dao.php
@@ -0,0 +1,54 @@
+<?php
+
+include '../../globals/configdb.php';
+
+function getProdutos() {
+    
+    $db = new SQLite3( '../../database/doacoespet.db' );
+    
+    $sql = "SELECT * FROM produto ORDER BY nome DESC";
+    $results = $db->query( $sql );
+    $arr = array();
+    $i = 0;
+    while ($row = $results->fetchArray()) {
+        $arr[$i] = $row;
+        $i++;  
+    }
+    
+    return $arr;
+    
+}
+
+function getDadosCentro($id) {
+    
+    $db = new SQLite3( '../../database/doacoespet.db' );
+    
+    $sql = "SELECT * FROM centro_apoio WHERE id='$id'";
+    $results = $db->query( $sql );
+    return $results->fetchArray();
+    
+    return $arr;
+    
+}
+
+
+function getDemandasCentro($centro_id) {
+    
+    $db = new SQLite3( '../../database/doacoespet.db' );
+    
+    $sql = "SELECT p.nome, p.unidade_medida, d.quantidade FROM demanda d, produto p WHERE d.centro_id=$centro_id ORDER BY p.nome DESC";
+    $results = $db->query( $sql );
+    $arr = array();
+    $i = 0;
+    while ($row = $results->fetchArray()) {
+        $arr[$i] = $row;
+        $i++;  
+    }
+    
+    return $arr;
+    
+}
+
+
+    
+?>
--- user/view/user_main.php
+++ user/view/user_main.php
@@ -1,3 +1,28 @@
+<?php
+    include 'isauthuser.php';
+    include 'header.php';
+    header('Content-Type: text/html; charset=utf-8');
+   include '../model/user_dao.php';
 
+?>	
 
+<html>
+<title>Página principal</title>
+<h1>Consultar Demandas</h1>
 
+<form action="user_get_demanda_centro.php" method="GET">
+  Centro de Apoio:<br>
+  <select id="centro_id" name="centro_id">
+      <?php
+        $centros = getCentros();
+        foreach ($centros as $centro ) {
+            $id = $centro['id'];
+            $nome = $centro['nome'];
+            echo "<option value=\"$id\">$nome</option>";
+        }
+      ?>
+  </select><br>
+  <input type="submit"  value="Consultar">
+</form><br>
+  
+</html>
