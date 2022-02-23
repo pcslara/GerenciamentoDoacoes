35665eea53c69ffc16f8faafbd99ae9c22fdf885
Douglas Cardoso <douglascardoso@cos.ufrj.br>
2022-02-23 10:41:22 -0300

    fixing header modification warning

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
+<?php
+if(!isset($_SESSION)) session_start();
+if (isset( $_SESSION['loginerrormsg'])) {
+    echo $_SESSION['loginerrormsg'];
+    unset( $_SESSION['loginerrormsg']);
+}
+session_destroy();
+?>  
 <html>
-    <?php
-        if(!isset($_SESSION)) session_start();
-        if (isset( $_SESSION['loginerrormsg'])) {
-            echo $_SESSION['loginerrormsg'];
-            unset( $_SESSION['loginerrormsg']);
-        }
-        session_destroy();
-    ?>  
 	    <form role="form" action="auth/user/auth_user.php" method="post">
 			<label  for="user">Username</label>
         	<input type="text" name="user" placeholder="Usernames..." id="form-username">
