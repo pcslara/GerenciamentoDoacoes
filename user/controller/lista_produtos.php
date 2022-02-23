c6d8660d53f7877396abd813b2d626be56ef7cef
Douglas Cardoso <douglascardoso@cos.ufrj.br>
2022-02-23 07:20:20 -0300

    quick fix for CORS issue

--- user/controller/lista_produtos.php
+++ user/controller/lista_produtos.php
@@ -1,5 +1,11 @@
 <?php
    include '../model/user_dao.php';
+   
    header('Content-Type: application/json');
+   
+   header('Access-Control-Allow-Origin: *');
+   header('Access-Control-Allow-Methods: GET, POST');
+   header("Access-Control-Allow-Headers: X-Requested-With");
+   
    echo json_encode(getProdutos());
 ?>
